0b446cb4afd0c59be308e334c7f8638adbfa9a24
Samu8912 <157401179+Samu8912@users.noreply.github.com>
2025-10-31 14:19:06 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - Configuración de Google Sheets API
+const CONFIG = {
+    API_KEY: 'AIzaSyDB0drcnkGdqKyJhsfStbcAQ7qiBqdhc4U',
+    CLIENT_ID: '965807081420-jc0gsh5umaief7s262qi22ooa64bvv14.apps.googleusercontent.com',
+    SPREADSHEET_ID: '1XJg_kvifS_e3UTztx7uhaNukCsnehuIF0ZhEC7lSFhw',
+    HOJA_BASE: 'BASE',
+    HOJA_ASISTENCIA: 'ASISTENCIA',
+    DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets'
+};
